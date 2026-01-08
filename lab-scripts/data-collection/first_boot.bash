b7414e690195092961dd94e7b33f7745e59de4d9
Darshen <118487376+Dimeany@users.noreply.github.com>
2026-01-08 14:11:09 -0600

    Update first_boot.bash
    added EM

--- lab-scripts/data-collection/first_boot.bash
+++ lab-scripts/data-collection/first_boot.bash
@@ -32,6 +32,7 @@ echo 'settings-update x123 ascii_settings MCAC=2048;THFA=22;THSL=0.378;GAIN=10;'
 fpga_ctrl='48000 3 4 60 48 65280 100 1092 610 0 0 17 512 0 16399 128'
 # fpga_ctrl registers: 
 # https://www.bridgeportinstruments.com/products/software/wxMCA_doc/documentation/english/mds/mca3k/mca3k_fpga_ctrl.html
+echo "settings-update hafx em fpga_ctrl $fpga_ctrl" > $det_udp_dev
 echo "settings-update hafx c1 fpga_ctrl $fpga_ctrl" > $det_udp_dev
 echo "settings-update hafx m1 fpga_ctrl $fpga_ctrl" > $det_udp_dev
 echo "settings-update hafx m5 fpga_ctrl $fpga_ctrl" > $det_udp_dev
@@ -42,6 +43,7 @@ echo "settings-update hafx x1 fpga_ctrl $fpga_ctrl" > $det_udp_dev
 # These may be extracted with the Bridgeport software
 arm_ctrl='0.0 0.0 1.0 1.0 2.0 0.10000000149011612 23.0 35.380001068115234 9000.0 20000.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0'
 # https://www.bridgeportinstruments.com/products/software/wxMCA_doc/documentation/english/mds/mca3k/mca3k_arm_ctrl.html
+echo "settings-update hafx em arm_ctrl $arm_ctrl" > $det_udp_dev
 echo "settings-update hafx c1 arm_ctrl $arm_ctrl" > $det_udp_dev
 echo "settings-update hafx m1 arm_ctrl $arm_ctrl" > $det_udp_dev
 echo "settings-update hafx m5 arm_ctrl $arm_ctrl" > $det_udp_dev
